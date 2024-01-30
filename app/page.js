dcdb4a17171a7b90bb6f1fbea9052ba2c75d9a76
JianHua-Deng <dengjianhua2397462613@gmail.com>
2024-01-29 21:55:36 -0500

    added temperary styling on start page

--- app/page.js
+++ app/page.js
@@ -4,12 +4,18 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        CodeFest Project
+      <div className={styles.startPageContainer}>
+        <div className={styles.description}>
+          <h1>CodeFest Project</h1>
+        </div>
+  
+        <div className={styles.container}>
+          <Link className={styles.linkComponent} href="/quiz">
+              <div className={styles.link}>Go to Quiz</div>
+          </Link>
+        </div>
       </div>
-      <Link href="/quiz">
-          <div className={styles.link}>Go to Quiz</div>
-      </Link>
+      
     </main>
   )
 }
