cccba767e42602d01904fc372feb1043dcd20c1e
JianHua-Deng <metails1203@gmail.com>
2024-01-31 16:37:39 -0500

    added word count function

--- app/flashcards/page.js
+++ app/flashcards/page.js
@@ -9,26 +9,32 @@ const [cardsGenerated, setCardGenerated] = useState(false);
 const [cardsData, setCards] = useState({});
 const [notes, setNotes] = useState("")
 const [numCard, setNumCard] = useState(5);
+const [wordCounts, updateCounts] = useState(0);
 
 const handleInput = (e) => {
-    setNotes(e.target.value)
+    setNotes(e.target.value);
+    updateCounts(e.target.value ? e.target.value.trim().split(" ").length : 0);
 }
 
 const handleClick = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("/api/ai/flashcard", {
-        method: "POST",
-        body: JSON.stringify({
-            notes: notes,
-            numCards: numCard
-        }),
-    });
-    const result = await response.json();
-    const cards = JSON.parse(result.completion.choices[0].message.content)
-    console.log(cards)//It prints the content fine in this way
-    setCards(cards);
-    console.log(cardsData);//It prints "[]" which is the default value
+    if(wordCounts < 100){
+        alert("Word count must be at least a 100!");
+    }else{
+        const response = await fetch("/api/ai/flashcard", {
+            method: "POST",
+            body: JSON.stringify({
+                notes: notes,
+                numCards: numCard
+            }),
+        });
+        const result = await response.json();
+        const cards = JSON.parse(result.completion.choices[0].message.content)
+        console.log(cards)//It prints the content fine in this way
+        setCards(cards);
+        console.log(cardsData);//It prints "[]" which is the default value
+    }
 }
 
 useEffect(() =>{
@@ -44,6 +50,7 @@ return(
                 <form className={styles.form} onSubmit={handleClick}>
                 <label>Enter Notes:</label>
                 <textarea className={styles.textarea} value={notes} onChange={handleInput}/>
+                <div className={styles.words}>Word Counts: {wordCounts}</div>
                 <button className={styles.button} type="submit">Submit Notes</button>
                 </form>
             </div>
