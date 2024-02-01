351ee4bc78021df41bb9647f03f7e50238fb4cd5
JianHua-Deng <metails1203@gmail.com>
2024-01-31 23:54:35 -0500

    fixed minor error message and added loading

--- app/flashcards/page.js
+++ app/flashcards/page.js
@@ -5,6 +5,8 @@ import {useState, useEffect} from "react";
 
 const Flashcards = () => {
 
+const [loading, setLoading] = useState(false);
+const [warning, setWarning] = useState("");
 const [cardsGenerated, setCardGenerated] = useState(false);
 const [cardsData, setCards] = useState({});
 const [notes, setNotes] = useState("")
@@ -20,37 +22,49 @@ const handleClick = async (e) => {
     e.preventDefault();
 
     if(wordCounts < 100){
-        alert("Word count must be at least a 100!");
+        setWarning("Insufficient word counts, please enter at least 100 words or else there may not be enough information for the API to generate cards");
     }else{
-        const response = await fetch("/api/ai/flashcard", {
-            method: "POST",
-            body: JSON.stringify({
-                notes: notes,
-                numCards: numCard
-            }),
-        });
-        const result = await response.json();
-        const cards = JSON.parse(result.completion.choices[0].message.content)
-        console.log(cards)//It prints the content fine in this way
-        setCards(cards);
-        console.log(cardsData);//It prints "[]" which is the default value
+        try{
+            setLoading(true);
+            setWarning("");
+            const response = await fetch("/api/ai/flashcard", {
+                method: "POST",
+                body: JSON.stringify({
+                    notes: notes,
+                    numCards: numCard
+                }),
+            });
+            const result = await response.json();
+            const cards = JSON.parse(result.completion.choices[0].message.content)
+            console.log(cards)//Printing out to see if the API returns in the right format
+            setCards(cards);
+        }catch(err){
+            console.log(err);
+        }finally{
+            setLoading(false);
+        }
     }
 }
 
 useEffect(() =>{
-    if(!(Object.keys(cardsData).length === 0)) setCardGenerated(true);
-    }, [cardsData])
+    if(!(Object.keys(cardsData).length === 0)){
+        setCardGenerated(true);
+    } }, [cardsData])
 
 return(
     <div className={styles.container}>
-        {!cardsGenerated ? (
-            
-            <div className={styles.formContainer}>
+        
+        {loading ? (
+            <div>Loading</div>
+        ) : (
+            !cardsGenerated ? (
+                <div className={styles.formContainer}>
                 <h2>Turning your notes into bunch of flashcards</h2>
                 <form className={styles.form} onSubmit={handleClick}>
                 <label>Enter Notes:</label>
                 <textarea className={styles.textarea} value={notes} onChange={handleInput}/>
                 <div className={styles.words}>Word Counts: {wordCounts}</div>
+                <div className={styles.insufficient}>{warning}</div>
                 <button className={styles.button} type="submit">Submit Notes</button>
                 </form>
             </div>
@@ -64,6 +78,8 @@ return(
                     ))}
                 </div>
             )
+            
+        )
         }
 
 
