4c0e5d2d70dd0a497669f99322937c325b85c02d
Daphne Luzong <daphneluzong@daphnes-air-4.mynetworksettings.com>
2024-02-04 14:57:30 -0500

    added navbar - wip

--- app/page.js
+++ app/page.js
@@ -1,26 +1,33 @@
 import styles from './page.module.css'
 import Link from 'next/link';
+import Navbar from './(components)/Navbar';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.startPageContainer}>
-        <div className={styles.description}>
-          <h1>CodeFest Project</h1>
-        </div>
-  
-        <div className={styles.linkContainer}>
-          <Link className={styles.linkComponent} href="/quiz">
-              <div className={styles.link}>Go to Quiz</div>
-          </Link>
-        </div>
+      <div>
+        <Navbar/>
+      </div>
 
-        <div className={styles.linkContainer}>
-          <Link className={styles.linkComponent} href="/flashcards">
-              <div className={styles.link}>Flashcards</div>
-          </Link>
-        </div>
+      <div className={styles.centerBox}>
+        <div className={styles.startPageContainer}>
+          <div className={styles.description}>
+            <h1>CodeFest Project</h1>
+          </div>
+    
+          <div className={styles.linkContainer}>
+            <Link className={styles.linkComponent} href="/quiz">
+                <div className={styles.link}>Go to Quiz</div>
+            </Link>
+          </div>
 
+          <div className={styles.linkContainer}>
+            <Link className={styles.linkComponent} href="/flashcards">
+                <div className={styles.link}>Flashcards</div>
+            </Link>
+          </div>
+        </div>
+      
       </div>
       
     </main>
