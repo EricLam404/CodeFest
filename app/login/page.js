577d32671b7d9511395dbd87ef9df040a19366d0
fatimasif <fatimasif2004@gmail.com>
2024-02-05 22:07:45 -0500

    added styling to login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,58 +1,5 @@
-// 'use client'
-
-// // pages/login.js
-// import React, { useState } from 'react';
-// import styles from './page.module.css'
-
-// const LoginPage = () => {
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-
-//   const handleUsernameChange = (event) => {
-//     setUsername(event.target.value);
-//   };
-
-//   const handlePasswordChange = (event) => {
-//     setPassword(event.target.value);
-//   };
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     console.log('Username:', username);
-//     console.log('Password:', password);
-//   };
-
-//   return (
-//     <div>
-//       <h1>Login Page</h1>
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <label htmlFor="username">Username:</label>
-//           <input
-//             type="text"
-//             id="username"
-//             value={username}
-//             onChange={handleUsernameChange}
-//           />
-//         </div>
-//         <div>
-//           <label htmlFor="password">Password:</label>
-//           <input
-//             type="password"
-//             id="password"
-//             value={password}
-//             onChange={handlePasswordChange}
-//           />
-//         </div>
-//         <button type="submit">Login</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default LoginPage;
-
 'use client'
+
 // pages/login.js
 import React, { useState } from 'react';
 import styles from './page.module.css';
@@ -78,37 +25,42 @@ const LoginPage = () => {
   return (
     <div className={styles.container}>
       <h1>Login Page</h1>
-      <form className={styles.formContainer} onSubmit={handleSubmit}>
-        <div>
-          <label className={styles.label} htmlFor="username">
-            Username:
-          </label>
-          <input
-            className={styles.input}
-            type="text"
-            id="username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-        </div>
-        <div>
-          <label className={styles.label} htmlFor="password">
-            Password:
-          </label>
-          <input
-            className={styles.input}
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button className={styles.button} type="submit">
-          Login
-        </button>
-      </form>
+        <p className={styles.smallText}>Please enter your username and password to create an account and save your work</p>
+      <div className={styles.formContainer}>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label className={styles.label} htmlFor="username">
+              Username:
+            </label>
+            <input
+              className={styles.input}
+              type="text"
+              id="username"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </div>
+          <div>
+            <label className={styles.label} htmlFor="password">
+              Password:
+            </label>
+            <input
+              className={styles.input}
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <button className={styles.button} type="submit">
+            Create Account
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
 
 export default LoginPage;
+
+
