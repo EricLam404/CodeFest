e0f1fc4861ce9a22d51e8157a6a951391ded8737
EricLam404 <lameric9888@gmail.com>
2024-02-06 12:46:44 -0500

    Add message and display profile

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,22 +1,30 @@
 'use client';
 
-import { useUser } from '@auth0/nextjs-auth0/client';
+import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
+import Loading from '../(components)/Loading';
+import ErrorMessage from '../(components)/ErrorMessage';
 
-export default function ProfileClient() {
+const Profile = () => {
   const { user, error, isLoading } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
-  console.log(user)
 
   return (
     user && (
       <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
         <a href="/api/auth/logout">Logout</a>
+        {user?.picture && <img src={user.picture} alt={user?.name} />}
+        <h2>{user?.name}</h2>
+        <ul>
+        {Object.keys(user).map((objKey, i) => <li key={i}>{objKey}: {user[objKey]} </li>)}
+        </ul>
       </div>
     )
   );
 }
+
+export default withPageAuthRequired(Profile, {
+    onRedirecting: () => <Loading />,
+    onError: (error) => <ErrorMessage>{error.message}</ErrorMessage>,
+});
