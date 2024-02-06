e000730ef7732f2979a383bfc81143a616eee7ae
fatimasif <fatimasif2004@gmail.com>
2024-02-05 22:24:17 -0500

    added styling to login page

--- app/login/page.js
+++ app/login/page.js
@@ -24,43 +24,43 @@ const LoginPage = () => {
 
   return (
     <div className={styles.container}>
-      <h1>Login Page</h1>
+      <div className={styles.wholeBox}>
+        <h1 className={styles.header}>Login Page</h1>
         <p className={styles.smallText}>Please enter your username and password to create an account and save your work</p>
-      <div className={styles.formContainer}>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label className={styles.label} htmlFor="username">
-              Username:
-            </label>
-            <input
-              className={styles.input}
-              type="text"
-              id="username"
-              value={username}
-              onChange={handleUsernameChange}
-            />
-          </div>
-          <div>
-            <label className={styles.label} htmlFor="password">
-              Password:
-            </label>
-            <input
-              className={styles.input}
-              type="password"
-              id="password"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-          </div>
-          <button className={styles.button} type="submit">
-            Create Account
-          </button>
-        </form>
+        <div className={styles.formContainer}>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label className={styles.label} htmlFor="username">
+                Username:
+              </label>
+              <input
+                className={styles.input}
+                type="text"
+                id="username"
+                value={username}
+                onChange={handleUsernameChange}
+              />
+            </div>
+            <div>
+              <label className={styles.label} htmlFor="password">
+                Password:
+              </label>
+              <input
+                className={styles.input}
+                type="password"
+                id="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
+            <button className={styles.button} type="submit">
+              Create Account
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
 };
 
 export default LoginPage;
-
-
